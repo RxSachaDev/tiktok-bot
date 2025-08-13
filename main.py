1c40407c8df0f72e67425648d7a06b18d4117a65
RxSachaDev <sacha.work38@gmail.com>
2025-08-13 02:25:24 +0200

    Correction du main qui nettoie le dossier des résultats de compatibilité

--- main.py
+++ main.py
@@ -38,13 +38,13 @@ email_sender.send_folder_contents(
 
 # Astrology compatibility
 
-folder = "./results/compatibility_result"
-for filename in os.listdir(folder):
-    file_path = os.path.join(folder, filename)
-    if os.path.isfile(file_path) or os.path.islink(file_path):
-        os.unlink(file_path)
-    elif os.path.isdir(file_path):
-        shutil.rmtree(file_path)
+# folder = "./results/compatibility_result"
+# for filename in os.listdir(folder):
+#     file_path = os.path.join(folder, filename)
+#     if os.path.isfile(file_path) or os.path.islink(file_path):
+#         os.unlink(file_path)
+#     elif os.path.isdir(file_path):
+#         shutil.rmtree(file_path)
 
 compatibility_service = CompatibilityService().generate_content()
 for compatibility in compatibility_service:
